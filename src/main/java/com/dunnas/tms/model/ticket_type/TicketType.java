78ff2b065634958bd0f44f825e4a07420883caea
Yan Balbino <yanbnogueira@hotmail.com>
2026-04-13 10:07:35 -0300

    feat: Entity TicketType

---
+++ src/main/java/com/dunnas/tms/model/ticket_type/TicketType.java
@@ -0,0 +1,40 @@
+package com.dunnas.tms.model.ticket_type;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Table(name = "ticket_type")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+public class TicketType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 150)
+    private String title;
+
+    @Column(name = "deadline_days", nullable = false)
+    private Integer deadlineDays;
+}
