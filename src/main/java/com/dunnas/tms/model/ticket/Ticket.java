a21075fea47aa620f1e79ab47150b5101e595a4b
Yan Balbino <yanbnogueira@hotmail.com>
2026-04-13 10:01:33 -0300

    first commit

---
+++ src/main/java/com/dunnas/tms/model/ticket/Ticket.java
@@ -0,0 +1,89 @@
+package com.dunnas.tms.model.ticket;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dunnas.tms.model.attachment.Attachment;
+import com.dunnas.tms.model.base.BaseEntity;
+import com.dunnas.tms.model.comment.Comment;
+import com.dunnas.tms.model.ticket_status.TicketStatus;
+import com.dunnas.tms.model.ticket_type.TicketType;
+import com.dunnas.tms.model.unit.Unit;
+import com.dunnas.tms.model.user.UserAccount;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "ticket")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
+@ToString
+public class Ticket extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @ToString.Exclude 
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    @Column(name = "title", nullable = false, length = 150)
+    private String title;
+
+    @NotBlank(message = "Description is required")
+    @Column(name = "description", nullable = false, length = 300)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "status_id", nullable = false)
+    private TicketStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author_id", nullable = false)
+    private UserAccount author;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "unit_id", nullable = false)
+    private Unit unit;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "ticket_type_id", nullable = false)
+    private TicketType ticketType;
+
+    @Column(name = "completed_at")
+    private OffsetDateTime completedAt;
+
+    @Column(name = "due_date")
+    private OffsetDateTime dueDate;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude private List<Comment> comments = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude private List<Attachment> attachments = new ArrayList<>();
+}
---
+++ src/main/java/com/dunnas/tms/model/ticket_status/TicketStatus.java
@@ -0,0 +1,46 @@
+package com.dunnas.tms.model.ticket_status;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "ticket_status")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+public class TicketStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "hex_color", length = 7)
+    private String hexColor;
+
+    @Column(name = "is_default")
+    private Boolean isDefault;
+
+    @Column(name = "is_finalizer")
+    private Boolean isFinalizer;
+}
