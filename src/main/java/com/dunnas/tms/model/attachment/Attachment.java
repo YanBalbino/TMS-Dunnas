1161f4676725d36094ac4a14b8d3bb5fabc57127
Yan Balbino <yanbnogueira@hotmail.com>
2026-04-13 18:10:44 -0300

    feat: attachment entity

---
+++ src/main/java/com/dunnas/tms/model/attachment/Attachment.java
@@ -0,0 +1,54 @@
+package com.dunnas.tms.model.attachment;
+
+import com.dunnas.tms.model.base.BaseEntity;
+import com.dunnas.tms.model.ticket.Ticket;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "attachment")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
+@ToString(exclude = "ticket")
+public class Attachment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotBlank(message = "File name is required")
+    @Column(name = "file_name", nullable = false, length = 255)
+    private String fileName;
+
+    @Column(name = "mime_type", length = 50)
+    private String mimeType;
+
+    @NotBlank(message = "Storage path is required")
+    @Column(name = "storage_path", nullable = false, length = 255)
+    private String storagePath;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "ticket_id", nullable = false)
+    private Ticket ticket;
+}
