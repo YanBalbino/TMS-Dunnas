bdf563f97a2f450d59a9588c71e54acbf6f6c4ce
Yan Balbino <yanbnogueira@hotmail.com>
2026-04-14 21:11:46 -0300

    feat: ticket repository

---
+++ src/main/java/com/dunnas/tms/feature/ticket/TicketRepository.java
@@ -0,0 +1,18 @@
+package com.dunnas.tms.feature.ticket;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    List<Ticket> findAllByUnitIdOrderByCreatedAtDesc(Long unitId);
+
+    List<Ticket> findAllByAuthorIdOrderByCreatedAtDesc(Long authorId);
+
+    List<Ticket> findAllByStatusIdOrderByCreatedAtDesc(Long statusId);
+
+    List<Ticket> findAllByTicketTypeIdOrderByCreatedAtDesc(Long ticketTypeId);
+
+    List<Ticket> findAllByTicketTypeIdInOrderByCreatedAtDesc(List<Long> ticketTypeIds);
+}
