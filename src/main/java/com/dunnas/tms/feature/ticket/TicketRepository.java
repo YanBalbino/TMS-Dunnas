3a74cf0c81f2431ec547fc526017bedf763d62fc
Yan Balbino <yanbnogueira@hotmail.com>
2026-04-16 21:09:40 -0300

    feat: new methods for humanized queries in JSP pages

--- src/main/java/com/dunnas/tms/feature/ticket/TicketRepository.java
+++ src/main/java/com/dunnas/tms/feature/ticket/TicketRepository.java
@@ -7,12 +7,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
     List<Ticket> findAllByUnitIdOrderByCreatedAtDesc(Long unitId);
+    List<Ticket> findAllByUnitNumberOrderByCreatedAtDesc(Integer unitNumber);
 
     List<Ticket> findAllByAuthorIdOrderByCreatedAtDesc(Long authorId);
+    List<Ticket> findAllByAuthorNameContainingIgnoreCaseOrderByCreatedAtDesc(String authorName);
 
     List<Ticket> findAllByStatusIdOrderByCreatedAtDesc(Long statusId);
+    List<Ticket> findAllByStatusNameContainingIgnoreCaseOrderByCreatedAtDesc(String statusName);
 
     List<Ticket> findAllByTicketTypeIdOrderByCreatedAtDesc(Long ticketTypeId);
+    List<Ticket> findAllByTicketTypeTitleContainingIgnoreCaseOrderByCreatedAtDesc(String ticketTypeTitle);
 
     List<Ticket> findAllByTicketTypeIdInOrderByCreatedAtDesc(List<Long> ticketTypeIds);
 
