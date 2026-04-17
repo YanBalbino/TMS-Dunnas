7c18546eebf63bfaec0e20aaf2c6d17cd6797ff6
Yan Balbino <yanbnogueira@hotmail.com>
2026-04-16 22:16:07 -0300

    feat: method for finding units for single resident

--- src/main/java/com/dunnas/tms/feature/unit/UnitRepository.java
+++ src/main/java/com/dunnas/tms/feature/unit/UnitRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UnitRepository extends JpaRepository<Unit, Long> {
 
     List<Unit> findAllByBlockIdOrderByFloorNumberAscNumberAsc(Long blockId);
+
+    List<Unit> findDistinctByResidentsIdOrderByFloorNumberAscNumberAsc(Long residentId);
 }
